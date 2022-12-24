eebfc41d829ffd0cc88b08e2f6b8e98134907cba
oumbarae hachmahocine <hachmaoui@hotmail.fr>
2022-12-24 19:13:53 +0100

    phase 14 :  ORM (ajout d'un anregistrement ).

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -2,11 +2,16 @@
 
 namespace App\Form;
 
+use App\Entity\Environnement;
 use App\Entity\Visite;
+use DateTime;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;¨V  <
+use Symfony\Component\OptionsResolver\OptionsResolver;101  
 
 class VisiteType extends AbstractType
 {
@@ -15,11 +20,15 @@ class VisiteType extends AbstractType
         $builder
             ->add('ville')
             ->add('pays')
-            ->add('datecreation',null,[
-                'widget'=> 'single_text',
-                'label'=>'date'
+             ->add('datecreation', DateType::class, [
+                'widget' => 'single_text',
+                'data' => isset($options['data']) &&
+                    $options['data']->getDateCreation() != null ? $options['data']->getDateCreation() : new DateTime('now'),
+                'label' => 'Date'
+            
             ])
             ->add('note')
+                
             ->add('avis')
             ->add('tempmin', null,[
                 'label'=>'t°min'
